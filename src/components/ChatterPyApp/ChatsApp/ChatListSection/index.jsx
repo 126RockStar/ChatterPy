5deb4ca809743a250bdcf91a60e51e9db40b5cb3
Andrej Petkovski <andrejaleksei86@osmicro.net.au>
2020-12-09 03:28:07 +1100

    infinite scroll

--- src/components/ChatterPyApp/ChatsApp/ChatListSection/index.jsx
+++ src/components/ChatterPyApp/ChatsApp/ChatListSection/index.jsx
@@ -4,6 +4,7 @@ import * as React from 'react';
 import ChatListContact from './ChatListContact';
 import useWindowResizeListener from '../../../../utils/useWindowResizeListener';
 import type { ChatEvent, Contact, ContactId, Conversation } from '../../types';
+import InfiniteScroll from "react-infinite-scroll-component";
 
 type Props = {
   /**
@@ -29,8 +30,21 @@ export default function ChatListSection({
   const [mobileChatListHeight, setMobileChatListHeight] = React.useState(
     undefined,
   );
+
+  const [infiniteScrollUsers, setInfinieScrollUsers] = React.useState([]);
+  const [hasMore, setHasMore] = React.useState(false);
+
   const desktopChatListRef = React.useRef();
   const mobileChatListRef = React.useRef();
+
+  React.useEffect(()=>{
+    let filterUsers = contactsWithConversations.filter((item, index)=>index<5);
+    console.log('filterusers1', contactsWithConversations );
+    if (contactsWithConversations.length>5) {
+      setHasMore(true);
+    }
+    setInfinieScrollUsers(filterUsers);
+  }, [contactsWithConversations])
 
   const setChatListHeights = React.useCallback(() => {
     if (desktopChatListRef.current) {
@@ -99,10 +113,60 @@ export default function ChatListSection({
       </div>
     </div>
   );
+  
+  const fetchMoreData = () => {
+    setTimeout(() => {
+      let filterUsers = contactsWithConversations.filter((item, index)=>index>=infiniteScrollUsers.length&&index<2*infiniteScrollUsers.length);
+      if (filterUsers.length<5) {
+        setHasMore(false);
+      }
+      let newUsers = [...infiniteScrollUsers, ...filterUsers];
+      console.log('newUsers', filterUsers);
+      setInfinieScrollUsers(newUsers);
+    }, 500);
+  }
 
   return (
     <>
-      {desktopView}
+      <div className={`chats__sidebar hidden-xs`}>
+        <div
+          id="scrollableDiv"
+          style={{
+            height: 500,
+            overflowY: 'scroll',
+            display: 'flex',
+          }}
+        >
+            <InfiniteScroll
+              dataLength={infiniteScrollUsers.length}
+              next={fetchMoreData}
+              hasMore={hasMore}
+              scrollableTarget="scrollableDiv"
+            >
+              {
+                infiniteScrollUsers&&infiniteScrollUsers.map(([contact, conversation]) => {
+                  const lastMessageContents = conversation.newestMessage || '';
+                  return (
+                    <ChatListContact
+                      key={contact.id}
+                      contact={contact}
+                      lastMessageContents={lastMessageContents}
+                      lastMessageTime={conversation.newestTime}
+                      isActiveChat={
+                        selectedChatContact !== undefined &&
+                        selectedChatContact.id === contact.id
+                      }
+                      conversationId={conversation.id}
+                    />
+                  );
+                })
+              } 
+            </InfiniteScroll>            
+            </div>
+            {
+              hasMore?<h4 style={{textAlign:'center'}}>Loading...</h4>:null
+            }
+        </div>
       {mobileView}
     </>
   );
